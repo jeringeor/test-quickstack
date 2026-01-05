5c434ceb1cb08cf7ddf94954aa3d0819afaa7a4f
Jerin George <116091529+jeringeor@users.noreply.github.com>
2026-01-05 16:55:20 +0530

    Initialize Express server with basic route

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+const APP_ENV = process.env.APP_ENV || "dev";
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello from QuickStack 🚀",
+    env: APP_ENV,
+    time: new Date().toISOString()
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
